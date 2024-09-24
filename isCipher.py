7d5f8f0f178281d6f10d1d93cbc9cba04076fc30
William <willspri@gmail.com>
2024-09-24 13:59:59 +1200

    second graph added

--- isCipher.py
+++ isCipher.py
@@ -14,3 +14,40 @@ plt.grid(axis='y')
 
 plt.show()
 
+def letter_occurrences_percentage(input_string):
+
+    filtered_string = ''.join(filter(str.isalpha, input_string.lower()))
+    
+    total_letters = len(filtered_string)
+    
+    letter_count = {}
+    for letter in filtered_string:
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+
+    letter_percentage = {letter: (count / total_letters) * 100 for letter, count in letter_count.items()}
+
+    return letter_percentage
+
+input_string = "This is a long string with repeated letters in to see the percentage of each letter that is written. A longer sentence is probably better"
+percentages = letter_occurrences_percentage(input_string)
+print(percentages)
+
+input_string = "This is a long string with repeated letters in to see the percentage of each letter that is written. A longer sentence is probably better"
+percentages = letter_occurrences_percentage(input_string)
+print(percentages)
+
+second_values = [percentages.get(letter, 0) for letter in letters] 
+
+plt.figure(figsize=(10, 5)) 
+plt.bar(letters, second_values, color='orange')
+
+plt.title('Letter Frequencies from Input String')
+plt.xlabel('Letters')
+plt.ylabel('Frequencies (%)')
+plt.grid(axis='y')
+
+# Show the second plot
+plt.show()
