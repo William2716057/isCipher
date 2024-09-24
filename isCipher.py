0d2caf945cd625bb0db2a5dfd02d88a9d40019c5
William <willspri@gmail.com>
2024-09-24 13:45:04 +1200

    tidied

--- isCipher.py
+++ isCipher.py
@@ -4,17 +4,13 @@ import numpy as np
 letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]  
 values = [8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.15, 0.77, 4.0, 2.4, 6.7, 7.5, 1.9, 0.095, 6.0, 6.3, 9.1, 2.8, 0.98, 2.4, 0.15, 2.0, 0.074]
 
-
 plt.bar(letters, values, color='blue')
-
 
 plt.title('Expected letter frequences')
 plt.xlabel('Letters')
 plt.ylabel('Frequences')
 
-# Show grid
 plt.grid(axis='y')
 
-# Display the plot
 plt.show()
-plt.show()
+
